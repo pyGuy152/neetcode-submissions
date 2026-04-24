826054653bdf0e1adb99498e89f47529f425d806
Sarvesh Sabe <163082947+pyGuy152@users.noreply.github.com>
2026-04-23 19:07:10 -0500

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if (len(strs) == 0):
+            return "none"
+        return "/?".join(strs)
+        
+    def decode(self, s: str) -> List[str]:
+        if (s == "none"):
+            return []
+        return s.split("/?")
